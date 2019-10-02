0376394aa37f5701928b7efe517ea47e956c4f85
Adrian Tobias <adrian.tobias@oocl.com>
2019-10-02 14:57:45 +0800

    Completed exercise in jQuery

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,129 @@
 $(document)
     .ready(function () {
 
+        let arrayOfToDoItems = [];
+        let defaultFilter = "all";
+        $("#filters li a[data-filter=all]").addClass("selected");
+
+        function addItem() {
+            let stringFromField = $('input[name=ListItem]').val();
+
+            let toDo = {
+                id :  generateUUID(),
+                name : stringFromField,
+                complete : false
+            };
+
+            arrayOfToDoItems.push(toDo);
+            buildItemList();
+
+            $('input[name=ListItem]').val("");
+
+        }
+
+        function buildItemList() {
+            if(defaultFilter === "all"){
+                $("#List").empty();
+                arrayOfToDoItems.forEach(function(item){
+                    let li = document.createElement('li');
+                    li.id = item.id;
+                    li.name = item.name;
+                    if(checkIfToDoIsCompleted(item.complete)){
+                        li.style.textDecoration = "line-through";
+                        li.style.color = "#999";
+                        li.innerHTML = '<input name = "done-todo" type="checkbox" class="done-todo" checked/><span>' + item.name + '</span>';
+                    }else{
+                        li.innerHTML = '<input name = "done-todo" type="checkbox" class="done-todo" /><span>' + item.name + '</span>';
+                    }
+                    document.getElementById("List").appendChild(li);
+                });
+            }
+            if(defaultFilter === "complete"){
+                $("#List").empty();
+                arrayOfToDoItems.forEach(function(item){
+                    if(item.complete === true){
+                        let li = document.createElement('li');
+                        li.id = item.id;
+                        li.name = item.name;
+                        if(checkIfToDoIsCompleted(item.complete)){
+                            li.style.textDecoration = "line-through";
+                            li.style.color = "#999";
+                            li.innerHTML = '<input name = "done-todo" type="checkbox" class="done-todo" checked/><span>' + item.name + '</span>';
+                        }else{
+                            li.innerHTML = '<input name = "done-todo" type="checkbox" class="done-todo" /><span>' + item.name + '</span>';
+                        }
+                        document.getElementById("List").appendChild(li);
+                    }
+                });
+            }
+            if(defaultFilter === "active"){
+                $("#List").empty();
+                arrayOfToDoItems.forEach(function(item){
+                    if(item.complete === false){
+                        let li = document.createElement('li');
+                        li.id = item.id;
+                        li.name = item.name;
+                        if(checkIfToDoIsCompleted(item.complete)){
+                            li.style.textDecoration = "line-through";
+                            li.style.color = "#999";
+                            li.innerHTML = '<input name = "done-todo" type="checkbox" class="done-todo" checked/><span>' + item.name + '</span>';
+                        }else{
+                            li.innerHTML = '<input name = "done-todo" type="checkbox" class="done-todo" /><span>' + item.name + '</span>';
+                        }
+                        document.getElementById("List").appendChild(li);
+                    }
+                });
+            }
+            ;
+        }
+
+        $(document).on('dblclick', 'li', function () {
+            $(this)
+                .children('span')
+                .attr('contentEditable', 'true')
+                .focus()
+                .keypress(function (event) {
+                    var keycode = (event.keyCode
+                        ? event.keyCode
+                        : event.which);
+                    if (keycode == '13') {
+                        event
+                            .target
+                            .blur();
+                        $(this)
+                            .children('span')
+                            .attr('contenteditable', 'false');
+
+                        arrayOfToDoItems[arrayOfToDoItems.findIndex(el => el.id === this.parentNode.id)].name = this.innerText;
+                        buildItemList();
+
+                    }
+                });
+        });
+
+
+        function checkIfToDoIsCompleted(item){
+            return (item) ? "checked" : "";
+        }
+
+        $(document).on('click', 'input[name=done-todo]', function (event) {
+            $(this)
+                .parent()
+                .toggleClass('checked');
+
+            arrayOfToDoItems[arrayOfToDoItems.findIndex(el => el.id === this.parentNode.id)].complete = !arrayOfToDoItems[arrayOfToDoItems.findIndex(el => el.id === this.parentNode.id)].complete;
+        buildItemList();
+        });
+
+        $('#filters li a').click(function(e){
+           e.preventDefault();
+           defaultFilter = $(this).data('filter');
+
+            $("#filters li a").removeClass("selected");
+            $(this).addClass("selected");
+
+           buildItemList();
+        });
         function generateUUID() {
             /*jshint bitwise:false */
             var i,
@@ -21,5 +144,6 @@ $(document)
             return uuid;
         }
 
-        // code to be implemented
+        $('#button').click(addItem);
+
     });
